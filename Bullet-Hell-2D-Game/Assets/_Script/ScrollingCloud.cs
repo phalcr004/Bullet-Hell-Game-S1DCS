240be84f5d77f79093035974fda7af66bbe13cdb
Phill Crosby <43401190+phalcr004@users.noreply.github.com>
2020-05-20 09:19:53 +0930

    begun work on cloud movement

---
+++ Bullet-Hell-2D-Game/Assets/_Script/ScrollingCloud.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingCloud : MonoBehaviour {
+    [SerializeField] float speed;
+    [SerializeField] float xBoundary = -15f;
+    [SerializeField] float yTopBoundary = 4.5f;
+    [SerializeField] float yBottomBoundary = -3f;
+
+    void Update() {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);       
+
+        if(transform.position.x < xBoundary) {
+            float randomYPosition = Random.Range(yBottomBoundary, yTopBoundary);
+            transform.position = new Vector2(-xBoundary, randomYPosition);
+        }
+    }
+}
