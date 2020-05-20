a785f19f0d33c1aa5750dfab6877e3f3972d1e91
Phill Crosby <43401190+phalcr004@users.noreply.github.com>
2020-05-20 11:58:49 +0930

    added spawn manager and spawn manager script

---
+++ Bullet-Hell-2D-Game/Assets/_Script/SpawnManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    [SerializeField] GameObject[] enemySpawnPoints;
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
