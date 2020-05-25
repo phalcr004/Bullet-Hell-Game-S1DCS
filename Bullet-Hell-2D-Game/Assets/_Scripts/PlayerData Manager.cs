42bc045160de6b5c03dbf9b340e56da54aa64afe
Phill Crosby <43401190+phalcr004@users.noreply.github.com>
2020-05-25 20:44:14 +0930

    begun work on high score system

---
+++ Bullet-Hell-2D-Game/Assets/_Scripts/PlayerData Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PlayerDataManager : MonoBehaviour {
+    private string path;
+    private string jsonString;
+
+    public static PlayerData playerData;
+    void Start() {
+        path = Application.streamingAssetsPath + "/player-data.JSON";
+        jsonString = File.ReadAllText(path);
+        playerData = JsonUtility.FromJson<PlayerData>(jsonString);
+    }
+
+    public static void OverwriteJsonFile() {
+        JsonUtility.ToJson(playerData);
+    }
+}
+
+[System.Serializable]
+public class PlayerData {
+    public Highscores[] highscores;
+}
+
+[System.Serializable]
+public class Highscores {
+    public int position;
+    public int score;
+}
