b7b177f02b2063d7f2d7b3585e9061f7a8e89684
Jack-Flaherty <60948758+Jack-Flaherty@users.noreply.github.com>
2020-05-27 10:31:14 +0930

    Music Added and Game Manager

--- Bullet-Hell-2D-Game/Assets/_Scripts/System/GameManager.cs
+++ Bullet-Hell-2D-Game/Assets/_Scripts/System/GameManager.cs
@@ -5,26 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public AudioClip[] music;
+    private AudioClip selectedMusic;
+    [SerializeField] AudioSource cameraAudio;
+    [SerializeField] AudioListener cameraVolume;
+    public AudioClip bossMusic;
+
     public bool isGameActive;
+    public bool activateBoss;
+    public bool isBossActive;
+
     // Start is called before the first frame update
     void Start()
     {
+        isBossActive = false;
+        ChooseMusic();
         isGameActive = true;
+        if(TitleButtons.gameAudio == false)
+        {
+            AudioListener.volume = 0f;
+        }
+        else
+        {
+            AudioListener.volume = .75f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (activateBoss == true)
+        {
+            activateBoss = false;
+            ActivateBoss();
+        }
     }
     public void GameOver()
     {
         isGameActive = false;
+        AudioListener.volume = 0.25f;
     }
-
-    public void ManageScene(int scene)
+    public void ChooseMusic()
     {
-        SceneManager.LoadScene(scene);
-        //SceneManager.LoadScene(0);
+        int musicChoice = Random.Range(0, music.Length);
+        cameraAudio.clip = music[musicChoice];
+        cameraAudio.Play();
+    }
+    public void ActivateBoss()
+    {
+        isBossActive =true;
+        cameraAudio.Stop();
+        cameraAudio.PlayOneShot(bossMusic,1f);
     }
 }
